5a3bc1d627a03a3103b72fcb3d40e55ead32ddd3
James Johnson <music1384@yahoo.co.uk>
2018-01-19 05:57:27 +0000

    Refactor movement and setup

--- Assets/Code/Classes/SnakeController.cs
+++ Assets/Code/Classes/SnakeController.cs
@@ -7,29 +7,33 @@ public class SnakeController : MonoBehaviour
 {
     [Tooltip ("The speed of the snake's movement on screen.")]
     [SerializeField] private float _Speed = 0.5f;
+    [Tooltip ("The prefab to use as the snake's tail.")]
     [SerializeField] private GameObject _TailPiecePrefab = null;
+    [Tooltip ("The snake's current tail size and objects.")]
     [SerializeField] private List<Transform> _Tail = new List<Transform> ();
 
-    private Vector3 _Direction = Vector2.zero;
+    private Vector2 _Direction = Vector2.zero;
     private Transform _Transform = null;
     private Rigidbody2D _Rigidbody2D = null;
 
     private void Awake ()
     {
+        AssignReferences ();
+        SetupRigidbody ();
+    }
+
+    private void AssignReferences ()
+    {
         _Transform = GetComponent<Transform> ();
         _Rigidbody2D = GetComponent<Rigidbody2D> ();
         GetComponent<Collider2D> ().isTrigger = true;
+    }
 
+    private void SetupRigidbody ()
+    {
         _Rigidbody2D.gravityScale = 0.0f;
         _Rigidbody2D.isKinematic = true;
         _Rigidbody2D.freezeRotation = true;
-
-        for (int i = 0; i < 10; i++)
-        {
-            var piece = Instantiate (_TailPiecePrefab, Vector3.zero, Quaternion.identity);
-
-            _Tail.Add (piece.transform);
-        }
     }
 
     private void Start ()
@@ -46,30 +50,39 @@ public class SnakeController : MonoBehaviour
     {
         // Get the snakes next movement direction ensuring that it can never turn back on itself thus causing instant death.
 
-        if (Input.GetAxisRaw ("Horizontal") > 0.0f && _Direction != Vector3.left)
-            _Direction = Vector3.right;
-        else if (Input.GetAxisRaw ("Horizontal") < 0.0f && _Direction != Vector3.right)
-            _Direction = Vector3.left;
-        else if (Input.GetAxisRaw ("Vertical") > 0.0f && _Direction != Vector3.down)
-            _Direction = Vector3.up;
-        else if (Input.GetAxisRaw ("Vertical") < 0.0f && _Direction != Vector3.up)
-            _Direction = Vector3.down;
+        if (Input.GetAxisRaw ("Horizontal") > 0.0f && _Direction != Vector2.left)
+            _Direction = Vector2.right;
+        else if (Input.GetAxisRaw ("Horizontal") < 0.0f && _Direction != Vector2.right)
+            _Direction = Vector2.left;
+        else if (Input.GetAxisRaw ("Vertical") > 0.0f && _Direction != Vector2.down)
+            _Direction = Vector2.up;
+        else if (Input.GetAxisRaw ("Vertical") < 0.0f && _Direction != Vector2.up)
+            _Direction = Vector2.down;
     }
 
     private IEnumerator Move ()
     {
         yield return new WaitForSeconds (_Speed);
 
-        var currentPos = _Transform.position;
-        var endPiece = _Tail[_Tail.Count - 1];
-        _Tail.RemoveAt (_Tail.Count - 1);
-
-        endPiece.position = currentPos;
-        _Tail.Insert (0, endPiece);
-
-        _Rigidbody2D.MovePosition (_Rigidbody2D.position + (Vector2)_Direction);
+        MoveTail ();
+        MoveHead ();
 
         StopCoroutine (Move ());
         StartCoroutine (Move ());
+    }
+
+    private void MoveTail ()
+    {
+        var currentHeadPosition = _Transform.position;
+        var lastTailPiece = _Tail[_Tail.Count - 1];
+        _Tail.RemoveAt (_Tail.Count - 1);
+
+        lastTailPiece.position = currentHeadPosition;
+        _Tail.Insert (0, lastTailPiece);
+    }
+
+    private void MoveHead ()
+    {
+        _Rigidbody2D.MovePosition (_Rigidbody2D.position + _Direction);
     }
 }
