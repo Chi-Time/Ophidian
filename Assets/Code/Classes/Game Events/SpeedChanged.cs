af956455e1e9de9a4e4fc3ac2c8f671cde5633db
James Johnson <music1384@yahoo.co.uk>
2018-01-19 08:37:29 +0000

    Implement and edit speed pickups

---
+++ Assets/Code/Classes/Game Events/SpeedChanged.cs
@@ -0,0 +1,9 @@
+﻿public class SpeedChanged : GameEvent
+{
+    public float _Speed = 0.0f;
+
+    public SpeedChanged (float speed)
+    {
+        _Speed = speed;
+    }
+}
---
+++ Assets/Code/Classes/Pickups/Speed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Speed : TimedPickup
+{
+    [SerializeField] private float _Speed = 0.0f;
+
+    protected override void Collect (Collider2D other)
+    {
+        EventManager.Instance.Raise (new SpeedChanged (_Speed));
+
+        base.Collect (other);
+    }
+
+    protected override void EndEffect ()
+    {
+        EventManager.Instance.Raise (new SpeedChanged (-_Speed));
+    }
+}
