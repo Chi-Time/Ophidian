c75404b35cd250313cf83c6466f78cc041ec26a4
James Johnson <music1384@yahoo.co.uk>
2018-01-19 08:53:01 +0000

    Implement pickup effects to snakecontroller

--- Assets/Code/Classes/Game Events/SpeedChanged.cs
+++ Assets/Code/Classes/Game Events/SpeedChanged.cs
@@ -1,9 +1,9 @@
 ﻿public class SpeedChanged : GameEvent
 {
-    public float _Speed = 0.0f;
+    public float Speed = 0.0f;
 
     public SpeedChanged (float speed)
     {
-        _Speed = speed;
+        Speed = speed;
     }
 }
--- Assets/Code/Classes/SnakeController.cs
+++ Assets/Code/Classes/SnakeController.cs
@@ -12,6 +12,7 @@ public class SnakeController : MonoBehaviour
     [Tooltip ("The snake's current tail size and objects.")]
     [SerializeField] private List<Transform> _Tail = new List<Transform> ();
 
+    private bool _IsImmune = false;
     private Vector2 _Direction = Vector2.zero;
     private Transform _Transform = null;
     private Rigidbody2D _Rigidbody2D = null;
@@ -38,26 +39,16 @@ public class SnakeController : MonoBehaviour
 
     private void Start ()
     {
+        SubscribeToEvents ();
+
         StartCoroutine (Move ());
     }
 
-    private void Update ()
+    private void SubscribeToEvents ()
     {
-        GetInput ();
-    }
-
-    private void GetInput ()
-    {
-        // Get the snakes next movement direction ensuring that it can never turn back on itself thus causing instant death.
-
-        if (Input.GetAxisRaw ("Horizontal") > 0.0f && _Direction != Vector2.left)
-            _Direction = Vector2.right;
-        else if (Input.GetAxisRaw ("Horizontal") < 0.0f && _Direction != Vector2.right)
-            _Direction = Vector2.left;
-        else if (Input.GetAxisRaw ("Vertical") > 0.0f && _Direction != Vector2.down)
-            _Direction = Vector2.up;
-        else if (Input.GetAxisRaw ("Vertical") < 0.0f && _Direction != Vector2.up)
-            _Direction = Vector2.down;
+        EventManager.Instance.AddListener<SpeedChanged> (OnSpeedChanged);
+        EventManager.Instance.AddListener<TailShrunk> (OnTailShrunk);
+        EventManager.Instance.AddListener<ImmunityGranted> (OnImmunityGranted);
     }
 
     private IEnumerator Move ()
@@ -86,11 +77,63 @@ public class SnakeController : MonoBehaviour
         _Rigidbody2D.MovePosition (_Rigidbody2D.position + _Direction);
     }
 
+    private void Update ()
+    {
+        GetInput ();
+    }
+
+    private void GetInput ()
+    {
+        // Get the snakes next movement direction ensuring that it can never turn back on itself thus causing instant death.
+
+        if (Input.GetAxisRaw ("Horizontal") > 0.0f && _Direction != Vector2.left)
+            _Direction = Vector2.right;
+        else if (Input.GetAxisRaw ("Horizontal") < 0.0f && _Direction != Vector2.right)
+            _Direction = Vector2.left;
+        else if (Input.GetAxisRaw ("Vertical") > 0.0f && _Direction != Vector2.down)
+            _Direction = Vector2.up;
+        else if (Input.GetAxisRaw ("Vertical") < 0.0f && _Direction != Vector2.up)
+            _Direction = Vector2.down;
+    }
+
     public void GrowTail ()
     {
         var piece = Instantiate (_TailPiecePrefab, Vector3.zero, Quaternion.identity).transform;
         piece.position = _Tail[_Tail.Count - 1].position;
 
         _Tail.Add (piece);
+    }
+
+    private void OnSpeedChanged (SpeedChanged e)
+    {
+        _Speed += e.Speed;
+    }
+
+    private void OnTailShrunk (TailShrunk e)
+    {
+        for (int i = 1; i < e.ShrinkAmount; i++)
+        {
+            var tailPiece = _Tail[_Tail.Count - i];
+            _Tail.Remove (tailPiece);
+
+            Destroy (tailPiece);
+        }
+    }
+
+    private void OnImmunityGranted (ImmunityGranted e)
+    {
+        _IsImmune = e.IsImmune;
+    }
+
+    private void OnDestroy ()
+    {
+        UnsubscribeFromEvents ();
+    }
+
+    private void UnsubscribeFromEvents ()
+    {
+        EventManager.Instance.RemoveListener<SpeedChanged> (OnSpeedChanged);
+        EventManager.Instance.RemoveListener<TailShrunk> (OnTailShrunk);
+        EventManager.Instance.RemoveListener<ImmunityGranted> (OnImmunityGranted);
     }
 }
